9a166d6d4627be5a2140a40e0f4754ef44c2fc05
RominaSaravia <61885187+RominaSaravia@users.noreply.github.com>
2024-06-11 23:46:53 +0000

    ejercicio en clase

---
+++ repaso/ejercicio.py
@@ -0,0 +1,25 @@
+#1
+class Vehiculo():
+    def __init__(self,color:str, patente:str, velocidad:int):
+        self.color = color
+        self.patente = patente
+        self.velocidad = velocidad
+    
+    def getColor(self):
+        return self.color
+
+    def calcularTiempoLlegada(self,distanciakm):
+        resultado = distanciakm / self.velocidad
+        return resultado
+
+
+vehiulo1 = Vehiculo("negro","ERT143",100)
+vehiulo2 = Vehiculo("blanco","EGH163",120)
+vehiulo3 = Vehiculo("rojo","EJU908",100)
+vehiulo4 = Vehiculo("azul","KRT153",80)
+vehiulo5 = Vehiculo("verde","JRT149",90)
+
+print(vehiulo1.getColor())
+print(vehiulo2.getColor())
+print(vehiulo3.calcularTiempoLlegada(1000))
+print(vehiulo5.calcularTiempoLlegada(1000))
