7a89b159c474a8b3097ec96724b765673c7bd3d4
RominaSaravia <61885187+RominaSaravia@users.noreply.github.com>
2024-06-13 23:40:22 +0000

    pruebas armando servidor

---
+++ myserver/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hola! servidor de prueba de la clase de Esp. en desarrollo Web!"}
+
+@app.get("/search")
+async def root(busqueda:str):
+    return f"La búsqueda es: '{busqueda}'"
+
+@app.get("/saludar/{nombre}")
+async def root(nombre:str):
+    return f'Hola, {nombre}, ¿Cómo estás?. \n Este es el servidor de Bruno.'
+
+@app.get("/obtenermayor")
+async def root(num1:int, num2:int):
+    numeroMayor=None
+    if num1 > num2: 
+        numeroMayor = num1
+    elif num1 < num2:
+        numeroMayor = num2
+    else:
+        return 'Los números son iguales.'    
+    return f'El número mayor es {numeroMayor}'
+
+@app.get("/sumar")
+async def root(num1:int, num2:int):
+    result=None
+    result = num1 + num2
+   
+    return f'El resultado es: {result}'
