597d9eb53e5d52ffb2417854cc60143dd04a03e0
RominaSaravia <61885187+RominaSaravia@users.noreply.github.com>
2024-06-18 23:40:35 +0000

    new endpoints

--- myserver/main.py
+++ myserver/main.py
@@ -11,7 +11,7 @@ async def root():
 async def root(busqueda:str):
     return f"La búsqueda es: '{busqueda}'"
 
-@app.get("/saludar/{nombre}")
+@app.get("/saludar")
 async def root(nombre:str):
     return f'Hola, {nombre}, ¿Cómo estás?. \n Este es el servidor de Bruno.'
 
@@ -32,3 +32,69 @@ async def root(num1:int, num2:int):
     result = num1 + num2
    
     return f'El resultado es: {result}'
+
+#################
+products = [
+    {"id":1,
+        "nombre": "mouse",
+     "category": "computacion"
+    },
+    {"id":2,
+        "nombre": "leche Tetra",
+     "category": "lacteos"
+     }
+
+]
+
+def filtrarPorCategoria(listaProductos):
+    prodFiltrados = []
+    for x in listaProductos:
+        if (x["category"] == "lacteos"):
+            prodFiltrados.append(x)
+        else:
+            pass
+    
+    return prodFiltrados
+
+def filtrarPorId(listaProductos, _id):
+    outPut = []
+    for x in listaProductos:
+        if (x["id"] == _id):
+            outPut.append(x)
+        else:
+            pass
+    
+    return outPut
+
+
+
+
+@app.post("/productos")
+async def root(product:dict ):
+    print("Los datos del producto son:", product)
+    products.append(product)
+
+    return {'message: Se creo producto'}
+
+@app.get("/productos")
+async def root():
+    return products
+
+@app.get("/productos/{category}")
+async def root(category:str):
+    print(f'Se filta por la categoria {category}')
+    return filtrarPorCategoria(products)
+
+
+@app.get("/producto/{id}")
+async def root(id:int):
+    
+    return filtrarPorId(products,id)
+
+
+
+
+
+
+
+
