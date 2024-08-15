4c38407eb3ec679a466dfe79072b5a6ad803debd
RominaSaravia <61885187+RominaSaravia@users.noreply.github.com>
2024-08-15 23:37:52 +0000

    moving files

--- ecommerce/server/main.py
+++ ecommerce/server/main.py
@@ -1,10 +1,16 @@
 from typing import Annotated
 from fastapi import FastAPI,Request, Response, Cookie
 from fastapi.responses import HTMLResponse
+from sqlmodel import Session,select,delete
+from db import Product,create_db_and_tables,engine
+
 
 app = FastAPI()
 
-productsList = [{'name':'teclado','price':15550,'img':''}]
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
 
 @app.get("/",response_class=HTMLResponse)
 async def read_items():
@@ -20,13 +26,42 @@ async def read_items():
     """
     return HTMLResponse(content=html_content, status_code=200)
 
-@app.get("/products")
-async def getProducts():
-    return productsList
-
 
 @app.post("/product")
-async def postProduct(productData: dict):
-    print('producto creado: ', productData)
-    productsList.append(productData)
-    return productsList
+def create_product(product: Product):
+    with Session(engine) as session:
+        session.add(product)
+        session.commit()
+        session.refresh(product)
+        return product
+
+
+@app.get("/products")
+def read_products():
+    with Session(engine) as session:
+        products = session.exec(select(Product)).all()
+        return products
+    
+@app.delete("/product/{id}")
+def delete_product(id:int):
+    with Session(engine) as session:
+        result = session.exec(delete(Product).where(Product.id == id))
+        session.commit()
+        return result
+
+
+#### ------------COOKIES---------------------
+
+@app.post("/auth")
+async def authenticate(body:dict, res:Response):
+    res.set_cookie('userId',body)
+    return 'Authenticated OK'
+
+
+
+@app.get("/read_cookie")
+async def get_cookie(my_var: Request):
+    print(f'Cookie: {my_var._cookies}')
+    return my_var._cookies
+
+
--- myserver/validations.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Product(BaseModel):
-    id: int
-    name: str | None = None
-    category: str | None = None
-    price: float
