63dedd1573bea1dbea4bdfb86d062513bc71d1c2
RominaSaravia <61885187+RominaSaravia@users.noreply.github.com>
2024-09-03 23:51:49 +0000

    get userId is from cookie, new login endpoint. EndPoints now check cookie, and get userId

--- ecommerce/server/main.py
+++ ecommerce/server/main.py
@@ -1,7 +1,7 @@
 from typing import Annotated
-from fastapi import FastAPI,Request, Response, Cookie
+from fastapi import FastAPI,Request, Response, Cookie, Form
 from sqlmodel import Session,select,delete
-from db import Product,Carts,create_db_and_tables,engine,readAllProducts,createNewProduct,deleteProduct,getCart,upsertCartProduct,deleteCartProduct
+from db import Product,Carts,create_db_and_tables,engine,readAllProducts,createNewProduct,deleteProduct,getCart,upsertCartProduct,deleteCartProduct,authUser
 
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -14,33 +14,42 @@ def on_startup():
     create_db_and_tables()
 
 
+@app.get("/login", response_class=HTMLResponse)
+async def read_item(request: Request, res:Response): 
+    return templates.TemplateResponse(
+        request=request, name="login.html", context={}
+    )
+
+
 
 templates = Jinja2Templates(directory="server/templates")
 @app.get("/seeproducts", response_class=HTMLResponse)
-async def read_item(request: Request,search:str=''):
+async def read_item(request: Request,res:Response,search:str=''):
+    userId = validateAuth(request._cookies)
+    if userId == False:
+        res.status_code = 401
+        return 'Error: not authorize'
+
     return templates.TemplateResponse(
         request=request, name="index.html", context={"productsList": readAllProducts(search)}
     )
 
-@app.get("/seecart/{userID}", response_class=HTMLResponse)
-async def read_item(request: Request,userID:int):
+@app.get("/seecart", response_class=HTMLResponse)
+async def read_item(request: Request, res:Response):
+    userId = validateAuth(request._cookies)
+    if userId == False:
+        res.status_code = 401
+        return 'Error: not authorize'
+    
     return templates.TemplateResponse(
-        request=request, name="cart.html", context={"cartDetails": getCart(userID)}
+        request=request, name="cart.html", context={"cartDetails": getCart(userId)}
     )
 
 @app.get("/",response_class=HTMLResponse)
-async def read_items():
-    html_content = """
-    <html>
-        <head>
-            <title>eShop</title>
-        </head>
-        <body>
-            <h1>Welcome! Are you buying?</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="home.html", context={}
+    )
 
 
 @app.post("/product")
@@ -64,38 +73,66 @@ async def get_products(search:str):
 def delete_product(id:int):
     return deleteProduct(id)
     
+#### --------------AUTH-----------------------------
+def validateAuth(cookies):
+    if 'cookieUserId' in cookies.keys():
+        return cookies['cookieUserId']
+    else:
+        return False
+
+@app.post("/login")
+async def validateLogin(email: Annotated[str, Form()] ,password: Annotated[str, Form()], res:Response):
+    userAuth = authUser(email, password)
+
+    if userAuth != None :
+        res.status_code = 200
+        res.set_cookie('cookieUserId', userAuth)
+        return 'Login exitoso'
+    else:
+        res.status_code = 404
+        return 'Error: email or password wrong. try again'
 
 
 
-#### ------------COOKIES---------------------
 
-@app.post("/auth")
-async def authenticate(body:dict, res:Response):
-    res.set_cookie('userId',body)
+#### ------------COOKIES----------------------------
+
+@app.get("/auth/{userId}")
+async def authenticate( res:Response, userId:int=1):
+    res.set_cookie('cookieUserId',userId)
     return 'Authenticated OK'
-
 
 
 @app.get("/read_cookie")
 async def get_cookie(my_var: Request):
     print(f'Cookie: {my_var._cookies}')
-    return my_var._cookies
+    userId = validateAuth(my_var._cookies)
+    return userId
 
 
-############# Carrito #################
+#### ------------ Carrito --------------------------
 @app.post("/cart")
-async def add_to_cart(cartProduct:dict):
-    print(f'*****************************')
+async def add_to_cart(cartProduct:dict, my_var: Request, res:Response):
+    userId = validateAuth(my_var._cookies)
+    if userId == False:
+        res.status_code = 401
+        return 'Error: not authorize'
+    
     if(cartProduct['amount'] == 0):
-        return deleteCartProduct(cartProduct['user_id'],cartProduct['product_id'])
+        return deleteCartProduct(userId,cartProduct['product_id'])
     else:
         print(f'cart created')
-        return upsertCartProduct(cartProduct['user_id'],cartProduct['product_id'],cartProduct['amount'])
+        return upsertCartProduct(userId,cartProduct['product_id'],cartProduct['amount'])
 
 
-@app.get("/cart/{userId}")
-async def get_cart_by_user(userId:int):
-    print(f'producto agregado al carrito del usuario "{userId}" --> id producto: ', userId)
+@app.get("/cart")
+async def get_cart_by_user(my_var: Request, res:Response):
+
+    userId = validateAuth(my_var._cookies)
+    if userId == False:
+        res.status_code = 401
+        return 'Error: not authorize'
+    
     carrito = getCart(userId)
     return carrito
 
